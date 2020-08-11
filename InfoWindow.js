142b018852794950697cb2f10fe706a6600ee933
Tarek Elkheir <elkheirt@wit.edu>
2020-08-11 18:45:51 -0400

    Made pop-up window a web component

---
+++ InfoWindow.js
@@ -0,0 +1,51 @@
+const template = document.createElement('template');
+template.innerHTML = `
+    <style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    }   
+    #wiki-container {
+        width: 400px;
+        height: 200px;
+        background-color: white;
+        color: black;
+        box-shadow: 0px 0px 10px 0px rgba(50, 50, 50, 0.75);
+        overflow: scroll;
+        position: fixed;
+        top: 0;
+        right: 0;
+        z-index: 2147483648;
+        scrollbar-width: none;
+        margin: 10px;
+        padding: 10px;
+        border-radius: 10px;
+    }
+
+    h1 {
+        margin-bottom: 5px;
+    }
+
+    p {
+        line-height: 1.5;
+    }
+
+    </style>
+    <div id="wiki-container">
+        <h1><slot name="heading"></h1>
+        <hr>
+        <p><slot name="text"></p>
+    </div>
+`;
+
+class InfoWindow extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+}
+
+window.customElements.define('info-window', InfoWindow);
--- draw.js
+++ draw.js
@@ -1,5 +1,55 @@
 chrome.runtime.sendMessage({from:"draw"});
+var scriptsLoaded = false;
+
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    
+    if(scriptsLoaded) {
+        drawWindow(message.message);
+    }
 });
+
+loadRes( chrome.extension.getURL("web-components-lite.js") ) // You may/may not need webcomponents.js here
+.then( loadRes( chrome.extension.getURL("InfoWindow.js") ) )
+.then(function() {
+    scriptsLoaded = true;
+});
+
+
+document.onclick = function() {
+    var toRemove = document.getElementById('wiki-ext');
+    if(toRemove) {
+        toRemove.parentNode.removeChild(toRemove);
+    }   
+}
+
+function loadRes(res) {
+    return new Promise(
+      function(resolve, reject) {
+        var link = document.createElement('script');
+        link.setAttribute('src', res);
+        link.onload = function() {
+          resolve(res);
+        };
+        document.head.appendChild(link);
+    });
+}
+
+
+function drawWindow(innerText) {
+    var infoWindow = document.createElement('info-window');
+    infoWindow.setAttribute('id','wiki-ext');
+    var headingElem = document.createElement('div');
+    headingElem.setAttribute('slot', 'heading')
+    headingElem.innerHTML = 'Heading'; // TODO: Change to actual heading from API
+
+    var pageText = document.createElement('div');
+    pageText.setAttribute('slot', 'text')
+    pageText.innerHTML = innerText;
+    infoWindow.appendChild(headingElem);
+    infoWindow.appendChild(pageText);
+
+    document.body.appendChild(infoWindow);
+}
+
+
+
