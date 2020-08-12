8d0f62f60ab990899ffce41d15b2471bb22725d8
Tarek Elkheir <elkheirt@wit.edu>
2020-08-12 14:10:28 -0400

    Added styling to popup.html

--- popup.js
+++ popup.js
@@ -1,18 +1,20 @@
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {message: "fetch"}, function(text) {
         if(!(text.msg === '')) {
-            // params.titles = text.msg; // TODO: sanitize input (replace spaces with %20)
-            // Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
-            // getWikiResponse(url).then(function(result) {
-            //     let articleIntro = extractSnippet(result);
-            //     displayArticleText(text.msg, articleIntro);
-            // });
-            displayArticleText('Heading','Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi reprehenderit sint dolorum qui fuga excepturi reiciendis adipisci aspernatur eveniet a dignissimos iusto deleniti aut, odio est molestias nulla porro sapiente similique quasi ut illo omnis. Quis id mollitia a? Natus cupiditate expedita quia molestias reprehenderit voluptatibus corporis consequuntur corrupti quas iusto? Aperiam, exercitationem optio hic ad beatae quaerat sit explicabo tenetur sint repellendus corrupti atque porro voluptates quas tempora aut tempore iusto vero. Officia doloremque accusantium ipsa quibusdam dolor quia, eaque itaque sequi aliquam commodi ullam nisi consequuntur sit. Iste accusamus blanditiis, esse voluptatibus omnis asperiores labore eius deleniti aut.');
+            params.titles = text.msg; // TODO: sanitize input (replace spaces with %20)
+            Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+            getWikiResponse(url).then(function(result) {
+                let articleIntro = extractSnippet(result);
+                displayText(text.msg, articleIntro);
+            });
+        }
+        else {
+            displayText('Error', 'No text selected.');
         }
     });
 });
 
-function displayArticleText(heading, text) {
+function displayText(heading, text) {
     var paragraphElem = document.getElementById('main-text');
     paragraphElem.innerText = text;
     var headingElem = document.getElementById('main-heading');
