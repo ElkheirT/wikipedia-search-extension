18cd3600174c9a13806f73b08a9cbe578f7c5e33
Tarek Elkheir <elkheirt@wit.edu>
2020-08-18 20:30:15 -0400

    Added search functionality and HTML templates

--- js/fetchinfo.js
+++ js/fetchinfo.js
@@ -1,8 +1,6 @@
-
 async function getAPIResponse (url) {
     const response = await fetch(url)
-    const jsonResponse = await response.json()
-    return jsonResponse
+    return response
 }
 
 function getAPIURL (parameters) {
@@ -14,28 +12,26 @@ function getAPIURL (parameters) {
     return url
 }
 
-function sortResults (data) {
-    data.sort(function (a, b) {
-        return a.index.localeCompare(b.index)
-    })
-    return data
-}
-
 function getSearchResultsData (response) {
-    var pageIDs = response.query.pageids
-    var pages = response.query.pages
-    var searchData = {search: []}
-    for (var i = 0; i < pageIDs.length; i++) {
-        if(!pages[pageIDs[i]].hasOwnProperty('pageprops')) {
-            var currentPage = {
-                index: pages[pageIDs[i]].index,
-                title: pages[pageIDs[i]].title,
-                text: pages[pageIDs[i]].extract
-            }
-            searchData.search.push(currentPage)
-        }
+    if(!response.hasOwnProperty('query')) {
+        throw new Error('No search results');
     }
-    return searchData
+    else {
+        var pageIDs = response.query.pageids
+        var pages = response.query.pages
+        var searchData = {search: []}
+        for (var i = 0; i < pageIDs.length; i++) {
+            if(!pages[pageIDs[i]].hasOwnProperty('pageprops')) {
+                var currentPage = {
+                    index: pages[pageIDs[i]].index,
+                    title: pages[pageIDs[i]].title,
+                    text: pages[pageIDs[i]].extract.trim()
+                }
+                searchData.search.push(currentPage)
+            }
+        }
+        return searchData
+    }
 }
 
 function getSearchResults (searchTerm) {
@@ -44,16 +40,16 @@ function getSearchResults (searchTerm) {
         redirects: '1',
         format: 'json',
         generator: 'search',
-        gsrlimit: '10',
+        gsrlimit: '5',
         prop: 'extracts|pageprops',
         ppprop: 'disambiguation',
+        exlimit: 'max',
         exintro: '',
         explaintext: '',
-        exlimit: 'max',
         indexpageids: '',
-        gsrsearch: 'intitle:'
+        gsrsearch: ''
     }
-    searchParameters.gsrsearch += searchTerm
+    searchParameters.gsrsearch = searchTerm
     var url = getAPIURL(searchParameters)
     var response = getAPIResponse(url) 
     return response
--- js/popup.js
+++ js/popup.js
@@ -1,29 +1,60 @@
 import { getSearchResults, getSearchResultsData } from './fetchInfo.js'
+import {html, render} from '../node_modules/lit-html/lit-html.js'
 
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { message: 'fetch' }, function (text) {
-        if (!(text.msg === '')) {
-            getSearchResults(text.msg).then(function (result) {
-                var data = getSearchResultsData(result)
-                renderTemplate(data)
-            })
-        } else {
-            displayArticleText('Error', 'No text selected.')
-        }
+        getResponse(text.msg)
     })
 })
 
-function displayArticleText (heading, text) {
-    document.getElementById('search-results-container').style.display = 'none'
-    document.getElementById('error-container').style.display = 'none'
-    var paragraphElem = document.getElementById('main-text')
-    paragraphElem.innerText = text
-    var headingElem = document.getElementById('main-heading')
-    headingElem.innerText = heading
+var searchBtn = document.getElementById('search-btn')
+var searchInput = document.getElementById('searchbar')
+searchBtn.onclick = function() {
+    var searchTerm = searchInput.value
+    getResponse(searchTerm)
 }
 
-function renderTemplate (data) {
-    var template = Handlebars.compile(document.getElementById('template').innerHTML)
-    var container = document.getElementById('container')
-    container.innerHTML = template(data)
+function getResponse(selectedtext) {
+    if (!(selectedtext === '')) {
+        var response = getSearchResults(selectedtext)
+        handleResponse(response, selectedtext)
+     }
+}
+
+function handleResponse(response, searchTerm) {
+    response.then(result => {
+        if(!result.ok) {
+            throw new Error('Issue communicating with Wikipedia API');
+        }
+        return result.json()
+    })
+    .then(jsonData => {
+        jsonData = getSearchResultsData(jsonData)
+        displaySearchResults(jsonData, searchTerm)
+    })
+    .catch(error => {
+        alert
+        displayError(error)
+    })
+}
+
+function displayError(error) {
+    var container = document.getElementById('main-content')
+    var template = html`<h1>Error</h1>
+                        <p>${error}</p>
+                        `
+    render(template, container)
+}
+
+function displaySearchResults (data, searchTerm) {
+    var container = document.getElementById('main-content')
+    var template = 
+        html`<h1>Search results for: \"${searchTerm}\"</h1>
+        ${data.search.map((item) => html`
+        <details>
+            <summary>${item.title}</summary>
+            <span>${item.text}</span>
+        </details>`)}
+`
+    render(template, container)
 }
